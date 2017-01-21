29e161dfd637ff5460c3e8e2ec01031a7d8c107a
Nevade86 <Nevade86@gmail.com>
2017-01-21 17:06:09 +0100

    Targettable

---
+++ Assets/Scripts/PlayerController/Targettable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Targettable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public void OnPointerEnter(PointerEventData data)
+    {
+        Debug.Log("Gaze Start Focus");
+
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+
+        Debug.Log("Gaze End Focus");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Gaze Click ");
+
+    }
+
+    public void OnPointerStay(PointerEventData eventData)
+    {
+        Debug.Log("Gaze Stay Focus");
+    }
+
+}
