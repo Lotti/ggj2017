f4bf92519dc5442fac419af21fa8e9bb067a65f6
Nevade86 <Nevade86@gmail.com>
2017-01-21 17:15:24 +0100

    Targettable 2

--- Assets/Scripts/PlayerController/Targettable.cs
+++ Assets/Scripts/PlayerController/Targettable.cs
@@ -4,21 +4,31 @@ using UnityEngine.EventSystems;
 
 public class Targettable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    public virtual void StartFocus()
+    { }
+
+    public virtual void EndFocus()
+    { }
+
+    public virtual void OnClick()
+    { }
+
     public void OnPointerEnter(PointerEventData data)
     {
         Debug.Log("Gaze Start Focus");
-
+        StartFocus();
     }
 
     public void OnPointerExit(PointerEventData data)
     {
-
         Debug.Log("Gaze End Focus");
+        EndFocus();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Gaze Click ");
+        OnClick();
 
     }
 
