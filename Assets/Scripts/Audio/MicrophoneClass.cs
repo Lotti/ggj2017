ef328919a3fa8e08a47991088187d777b8979d38
Augusto Pace <augusto.p@interactiveproject.it>
2017-01-21 21:37:59 +0100

    audio

---
+++ Assets/Scripts/Audio/MicrophoneClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneClass : MonoBehaviour
+{
+	private F1_MicStreamer streamer;
+
+    void Awake()
+    {
+		streamer = GetComponent<F1_MicStreamer> ();
+		streamer.floatsInDelegate = _cb_Streamer;
+    }
+
+	void _cb_Streamer( float[] samples ){
+		foreach ( var s in samples ) {
+			Debug.Log ( s );
+		}
+	}
+
+    void GetClip()
+    {
+        
+
+    }
+
+}
