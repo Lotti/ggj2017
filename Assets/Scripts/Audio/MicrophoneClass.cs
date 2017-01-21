e70a199ea005b7423830ae31bf0e344c6446faf0
Augusto Pace <augusto.p@interactiveproject.it>
2017-01-21 23:47:19 +0100

    Aggiunto sistema IntelliStrillo

--- Assets/Scripts/Audio/MicrophoneClass.cs
+++ Assets/Scripts/Audio/MicrophoneClass.cs
@@ -1,27 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class MicrophoneClass : MonoBehaviour
-{
-	private F1_MicStreamer streamer;
+public class MicrophoneClass : MonoBehaviour {
 
-    void Awake()
-    {
-		streamer = GetComponent<F1_MicStreamer> ();
-		streamer.floatsInDelegate = _cb_Streamer;
-    }
+	private AudioSource audioS;
+	private bool _isScreaming = false;
+	private string MicString = "Built-in Microphone";
+	int freq = 44100;
 
-	void _cb_Streamer( float[] samples ){
-		foreach ( var s in samples ) {
-			Debug.Log ( s );
+	public static event Action OnScream;
+
+	public float rmsRef = 1f;
+
+	void Awake () {
+		audioS = GetComponent<AudioSource> ();
+
+	}
+
+	void Start () {
+		audioS.clip = Microphone.Start ( MicString, true, 10, freq );
+		audioS.loop = true;
+		_isScreaming = false;
+		while ( !( Microphone.GetPosition ( MicString ) > 0 ) ) {
+		}
+		audioS.Play ();
+	}
+
+	void Update () {
+		float[] spectrum = new float[1024];
+		audioS.GetOutputData ( spectrum, 0 );
+		float m = 0;
+		foreach ( var s in spectrum ) {
+			m += s * s;
+		}
+		var rms = Mathf.Sqrt ( ( float )m / ( float )spectrum.Length );
+		var dB = 20 * Mathf.Log ( rms / rmsRef, 10f );
+		if ( dB >= -5f && !_isScreaming ) {
+			_isScreaming = true;
+			Debug.LogWarning ( "STRILLOOOOO!!!" );
+			if ( OnScream != null ) {
+				OnScream ();
+			}
+			StartCoroutine ( _WaitForScream() );
 		}
 	}
 
-    void GetClip()
-    {
-        
-
-    }
+	IEnumerator _WaitForScream () {
+		float timeStart = Time.time;
+		while ( ( Time.time - timeStart ) <= 2f ) {
+			yield return null;
+		}
+		Debug.LogWarning ( "XXX => STRILLOOOOO!!!" );
+		_isScreaming = false;
+	}
 
 }
