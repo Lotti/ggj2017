c64550829ebb084f4037a41d85dea5d971c9e7bc
Nevade86 <Nevade86@gmail.com>
2017-01-22 13:18:27 +0100

    ANCORA AUDIOCOSE

---
+++ Assets/PlayMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : Singleton<PlayMusic> {
+
+    void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(_instance);
+    }
+
+	// Use this for initialization
+	void Start () {
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
